a693e816ca34c83182027c8c93086c4314c69698
Kate Chamberlain <kate.chamberlain@softwire.com>
2026-09-14 15:31:45 +0100

    Passed tests - Queen moves diagonally and queen cannot make any other moves

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -11,25 +11,48 @@ namespace Chessington.GameEngine.Pieces
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
       
-            var CurrentSquare = board.FindPiece(this);
+            var currentSquare = board.FindPiece(this);
 
             var moves = new List<Square>();
 
+            // Lateral movement
             for (var i = 0; i < GameSettings.BoardSize; i++)
                 {
-                    if (i != CurrentSquare.Row)
+                    if (i != currentSquare.Row)
                     {
-                        moves.Add(Square.At(i, CurrentSquare.Col));
+                        moves.Add(Square.At(i, currentSquare.Col));
                     }
                 }
                 
                 for (var i = 0;i < GameSettings.BoardSize; i++)
                 {
-                    if (i != CurrentSquare.Col)
+                    if (i != currentSquare.Col)
                     {
-                        moves.Add(Square.At(CurrentSquare.Row, i));
+                        moves.Add(Square.At(currentSquare.Row, i));
                     }
                 }
+
+            // Diagonal movement
+            var directions = new List<(int RowChange, int ColChange)>
+            {
+                (1, 1),
+                (1, -1),
+                (-1, 1),
+                (-1, -1)
+            };
+
+            foreach (var direction in directions)
+            {
+                var row = currentSquare.Row + direction.RowChange;
+                var col = currentSquare.Col + direction.ColChange;
+
+                while (row >=0 && row < GameSettings.BoardSize && col >= 0 && col < GameSettings.BoardSize)
+                {
+                    moves.Add(Square.At(row, col));
+                    row += direction.RowChange;
+                    col += direction.ColChange;
+                }
+            }
 
             return moves;
         }
